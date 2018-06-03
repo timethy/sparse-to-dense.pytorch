c3c35f3b14c4b4baa421e354678f43b1170070a1
Tim Taubner <taubnert@ethz.ch>
2018-06-03 08:58:50 +0200

    Revert white d_min coloring (looks bad)

--- sparse_to_dense/utils.py
+++ sparse_to_dense/utils.py
@@ -5,16 +5,13 @@ from PIL import Image
 cmap = plt.cm.viridis
 
 
-def colored_depthmap(depth, d_min=None, d_max=None, white_d_min=True):
+def colored_depthmap(depth, d_min=None, d_max=None):
     if d_min is None:
         d_min = np.min(depth)
     if d_max is None:
         d_max = np.max(depth)
     depth_relative = (depth - d_min) / (d_max - d_min)
     colored = 255 * cmap(depth_relative)[:, :, :3] # H, W, C
-    if white_d_min:
-        # Set pixels with d_min to zero
-        colored[depth == d_min, :] = 255
     return colored
 
 
