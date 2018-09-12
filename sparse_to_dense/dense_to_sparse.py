fb26a482835ffddf81d9733528112ab1b5422467
Marius Fehr <marius.b.fehr@gmail.com>
2018-09-12 17:08:51 +0200

    add kinect noise function, weight contour picking by squared distance to sensor, canny on both rgb and depth

--- sparse_to_dense/dense_to_sparse.py
+++ sparse_to_dense/dense_to_sparse.py
@@ -1,5 +1,7 @@
 # coding=utf-8
+import matplotlib.pyplot as plt
 import numpy as np
+
 import cv2
 
 
@@ -8,7 +10,8 @@ def rgb2grayscale(rgb):
 
 
 class DenseToSparse:
-    def __init__(self):
+    def __init__(self, apply_kinect_noise=True):
+        self.apply_kinect_noise = apply_kinect_noise
         pass
 
     def dense_to_sparse(self, rgb, depth):
@@ -17,17 +20,37 @@ class DenseToSparse:
     def __repr__(self):
         pass
 
+    def apply_kinect_noise(self, depth):
+
+        print "applying kinect noise..."
+
+        width = depth.shape[0]
+        height = depth.shape[1]
+        for v in range(0, height):
+            for u in range(0, width):
+                depth_m = depth[u, v]
+                if depth_m > 0.0 and np.isfinite(depth_m):
+                    sigma = 0.0012 + 0.0019 * (depth_m - 0.4) * (depth_m - 0.4)
+                    depth[u, v] = np.random.normal(depth_m, sigma, 1)
+                    # print "mapped depth ", depth_m, " to noisy depth ", depth[
+                    #     u, v]
+
+        print "done."
+
+        return depth.copy()
+
 
 class UniformSampling(DenseToSparse):
     name = "uar"
 
-    def __init__(self, num_samples, max_depth=np.inf):
-        DenseToSparse.__init__(self)
+    def __init__(self, num_samples, max_depth=np.inf, apply_kinect_noise=True):
+        DenseToSparse.__init__(self, apply_kinect_noise)
         self.num_samples = num_samples
         self.max_depth = max_depth
 
     def __repr__(self):
-        return "%s{ns=%d,md=%.4f}" % (self.name, self.num_samples, self.max_depth)
+        return "%s{ns=%d,md=%.4f}" % (self.name, self.num_samples,
+                                      self.max_depth)
 
     def dense_to_sparse(self, rgb, depth):
         """
@@ -35,6 +58,9 @@ class UniformSampling(DenseToSparse):
         Only pixels with a maximum depth of `max_depth` are considered.
         If no `max_depth` is given, samples in all pixels
         """
+        if self.apply_kinect_noise:
+            depth = DenseToSparse.apply_kinect_noise(self, depth)
+
         if self.max_depth is not np.inf:
             mask_keep = depth <= self.max_depth
             n_keep = np.count_nonzero(mask_keep)
@@ -42,7 +68,9 @@ class UniformSampling(DenseToSparse):
                 return mask_keep
             else:
                 prob = float(self.num_samples) / n_keep
-                return np.bitwise_and(mask_keep, np.random.uniform(0, 1, depth.shape) < prob)
+                return np.bitwise_and(
+                    mask_keep,
+                    np.random.uniform(0, 1, depth.shape) < prob)
         else:
             prob = float(self.num_samples) / depth.size
             return np.random.uniform(0, 1, depth.shape) < prob
@@ -51,8 +79,13 @@ class UniformSampling(DenseToSparse):
 class SimulatedStereo(DenseToSparse):
     name = "sim_stereo"
 
-    def __init__(self, num_samples, max_depth=np.inf, dilate_kernel=0, dilate_iterations=0):
-        DenseToSparse.__init__(self)
+    def __init__(self,
+                 num_samples,
+                 max_depth=np.inf,
+                 dilate_kernel=0,
+                 dilate_iterations=0,
+                 apply_kinect_noise=True):
+        DenseToSparse.__init__(self, apply_kinect_noise)
         self.num_samples = num_samples
         self.max_depth = max_depth
         self.dilate_kernel = dilate_kernel
@@ -70,6 +103,9 @@ class SimulatedStereo(DenseToSparse):
     # Threshold the edge gradient
     # Dilatate
     def dense_to_sparse(self, rgb, depth):
+        if self.apply_kinect_noise:
+            depth = DenseToSparse.apply_kinect_noise(self, depth)
+
         gray = rgb2grayscale(rgb)
 
         depth_mask = np.bitwise_and(depth != 0.0, depth <= self.max_depth)
@@ -84,8 +120,10 @@ class SimulatedStereo(DenseToSparse):
         if non_zeros > 0:
             target_samples = 1.0 * self.num_samples / np.size(depth) * non_zeros
             edge_percentage = float(100 * target_samples) / np.size(depth)
-            min_mag_lower = np.percentile(mag[depth_mask], 100 - edge_percentage, interpolation='lower')
-            min_mag_upper = np.percentile(mag[depth_mask], 100 - edge_percentage, interpolation='higher')
+            min_mag_lower = np.percentile(
+                mag[depth_mask], 100 - edge_percentage, interpolation='lower')
+            min_mag_upper = np.percentile(
+                mag[depth_mask], 100 - edge_percentage, interpolation='higher')
             # print("Upper threshold %f with %d pixels above it" %
             #      (min_mag_upper, np.count_nonzero(np.bitwise_and(depth_mask, mag >= min_mag_upper))))
             # print("Lower threshold %f with %d pixels above it" %
@@ -95,26 +133,30 @@ class SimulatedStereo(DenseToSparse):
             missing_samples = target_samples - np.count_nonzero(mag_mask)
             # assert missing_samples >= 0
             #if missing_samples > 0:
-                # print("Missing %d samples" % missing_samples)
-                # If we take all of mag_fill_mask we have more than desired number of samples
+            # print("Missing %d samples" % missing_samples)
+            # If we take all of mag_fill_mask we have more than desired number of samples
             #    mag_fill_mask = np.bitwise_and(depth_mask, mag < min_mag_upper, mag >= min_mag_lower)
             #    idx_0, idx_1 = np.nonzero(mag_fill_mask)
             #    potential_samples = len(idx_0)
-                # print("Got %d potential samples" % potential_samples)
+            # print("Got %d potential samples" % potential_samples)
             #    if potential_samples > missing_samples:
-                    # We choose some random samples to fill in
+            # We choose some random samples to fill in
             #        index_of_indices = np.random.choice(len(idx_0), missing_samples, replace=False)
-                    # Set these randomly chosen potentials to 1
+            # Set these randomly chosen potentials to 1
             #        mag_mask[idx_0[index_of_indices], idx_1[index_of_indices]] = 1
-                    # The threshold is still the lowest possible
+            # The threshold is still the lowest possible
             #    else:
             #        mag_mask[idx_0, idx_1] = 1
-                    # print("filling up only partly possible, as all pixels with depth <= max_depth are already taken")
+            # print("filling up only partly possible, as all pixels with depth <= max_depth are already taken")
             #        assert np.all(mag_mask[depth_mask] == 1)
 
             if self.dilate_iterations >= 0:
-                kernel = np.ones((self.dilate_kernel, self.dilate_kernel), dtype=np.uint8)
-                cv2.dilate(mag_mask.astype(np.uint8), kernel, iterations=self.dilate_iterations)
+                kernel = np.ones(
+                    (self.dilate_kernel, self.dilate_kernel), dtype=np.uint8)
+                cv2.dilate(
+                    mag_mask.astype(np.uint8),
+                    kernel,
+                    iterations=self.dilate_iterations)
 
             return mag_mask
         else:
@@ -126,8 +168,12 @@ class SimulatedStereo(DenseToSparse):
 class SimulatedKinect(DenseToSparse):
     name = "sim_kinect"
 
-    def __init__(self, num_samples, weight_magnitude=1.0, weight_depth=1.0):
-        DenseToSparse.__init__(self)
+    def __init__(self,
+                 num_samples,
+                 weight_magnitude=1.0,
+                 weight_depth=1.0,
+                 apply_kinect_noise=True):
+        DenseToSparse.__init__(self, apply_kinect_noise)
         self.num_samples = num_samples
         self.weight_magnitude = weight_magnitude
         self.weight_depth = weight_depth
@@ -137,6 +183,9 @@ class SimulatedKinect(DenseToSparse):
                (self.name, self.num_samples, self.weight_magnitude, self.weight_depth)
 
     def dense_to_sparse(self, rgb, depth):
+        if self.apply_kinect_noise:
+            depth = DenseToSparse.apply_kinect_noise(self, depth)
+
         gray = rgb2grayscale(rgb)
 
         depth_mask = depth != 0.0
@@ -166,7 +215,11 @@ class SimulatedKinect(DenseToSparse):
             #print(np.max(probs))
             probs[~depth_mask] = 0.0
             probs /= np.sum(probs)
-            indices = np.random.choice(np.size(depth), size=self.num_samples, replace=False, p=probs.flatten())
+            indices = np.random.choice(
+                np.size(depth),
+                size=self.num_samples,
+                replace=False,
+                p=probs.flatten())
             assert len(indices) == len(set(indices))
 
             mask = np.zeros(np.size(depth), dtype=np.bool)
@@ -178,8 +231,12 @@ class SimulatedKinect(DenseToSparse):
 class Contours(DenseToSparse):
     name = "contours"
 
-    def __init__(self, num_samples, min_val=100, max_val=200):
-        DenseToSparse.__init__(self)
+    def __init__(self,
+                 num_samples,
+                 min_val=100,
+                 max_val=200,
+                 apply_kinect_noise=True):
+        DenseToSparse.__init__(self, apply_kinect_noise)
         self.num_samples = num_samples
         self.min_val = min_val
         self.max_val = max_val
@@ -189,6 +246,9 @@ class Contours(DenseToSparse):
                (self.name, self.num_samples, self.min_val, self.max_val)
 
     def dense_to_sparse(self, rgb, depth):
+        if self.apply_kinect_noise:
+            depth = DenseToSparse.apply_kinect_noise(self, depth)
+
         depth_mask = depth != 0.0
 
         non_zeros = np.count_nonzero(depth_mask)
@@ -197,26 +257,99 @@ class Contours(DenseToSparse):
             return depth_mask
         else:
             gray = rgb2grayscale(rgb)
+
+            # gray = depth
             blurred = cv2.GaussianBlur(gray, (5, 5), 0)
             gx = cv2.Sobel(blurred, cv2.CV_64F, 1, 0, ksize=5)
             gy = cv2.Sobel(blurred, cv2.CV_64F, 0, 1, ksize=5)
+            mag_rgb = cv2.magnitude(gx, gy)
 
-            mag = cv2.magnitude(gx, gy)
+            min_mag_upper_rgb = np.percentile(
+                mag_rgb[depth_mask], 60.0, interpolation='higher')
+            mag_mask_rgb = np.zeros(depth_mask.shape, np.uint8)
+            mag_mask_rgb[np.bitwise_and(mag_rgb >= min_mag_upper_rgb,
+                                        depth_mask)] = 255
 
-            target_samples = 1.0 * self.num_samples / np.size(depth) * non_zeros
-            edge_percentage = float(100 * target_samples) / np.size(depth)
-            min_mag_upper = np.percentile(mag[depth_mask], 100 - edge_percentage, interpolation='higher')
+            blurred = cv2.GaussianBlur(depth, (5, 5), 0)
+            gx = cv2.Sobel(blurred, cv2.CV_64F, 1, 0, ksize=5)
+            gy = cv2.Sobel(blurred, cv2.CV_64F, 0, 1, ksize=5)
+            mag_depth = cv2.magnitude(gx, gy)
 
-            mag_mask = np.zeros(depth_mask.shape, np.uint8)
-            mag_mask[np.bitwise_and(mag >= min_mag_upper, depth_mask)] = 255
+            min_mag_upper_depth = np.percentile(
+                mag_depth[depth_mask], 60.0, interpolation='higher')
+            mag_mask_depth = np.zeros(depth_mask.shape, np.uint8)
+            mag_mask_depth[np.bitwise_and(mag_depth >= min_mag_upper_depth,
+                                          depth_mask)] = 255
 
-            im2, contours, hierarchy = cv2.findContours(mag_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            chosen = np.random.choice([0, 1], size=len(contours), replace=True)
+            mag_mask = np.bitwise_or(mag_mask_depth, mag_mask_rgb)
 
-            #print(chosen)
+            fig = plt.figure()
+            fig.add_subplot(3, 1, 1)
+            plt.imshow(mag_mask_rgb)
+            fig.add_subplot(3, 1, 2)
+            plt.imshow(mag_mask_depth)
+            fig.add_subplot(3, 1, 3)
+            plt.imshow(mag_mask)
+            plt.show()
+
+            im2, contours, hierarchy = cv2.findContours(
+                mag_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            chosen_area = np.zeros(len(contours), dtype=bool)
+            areas = np.zeros(len(contours))
+            avg_depth = np.zeros(len(contours))
+            for i in range(0, len(contours)):
+                for contour_point in contours[i]:
+                    avg_depth[i] = avg_depth[i] + depth[contour_point[0][1],
+                                                        contour_point[0][0]]
+                avg_depth[i] = avg_depth[i] / len(contours[i])
+                areas[i] = cv2.contourArea(contours[i])
+
+                # Automatically select contours that have a low area, because
+                # it is basically mostly noise or double edges from canny
+                # edge detection.
+                chosen_area[i] = (areas[i] < 1000)
+
+            # print "avg_depth: ", avg_depth
+            # print "avg_depth_probabilities: ", avg_depth_probabilities
+            # print "norm avg_depth_probabilities: ", np.linalg.norm(
+            #     avg_depth_probabilities, ord=1)
+            # print "probs: ", len(avg_depth_probabilities)
+            # print "contours: ", len(contours)
+
+            # Choose contours at random with a probability that depends on the
+            # square of the distance to the camera.
+            avg_depth_squared = np.square(avg_depth)
+            contour_weights = np.subtract(
+                1.0, np.divide(avg_depth_squared, np.amax(avg_depth_squared)))
+
+            chosen_random = np.zeros(len(contours), dtype=bool)
+            for i in range(0, len(contours)):
+                chosen_random[i] = (np.random.random() < contour_weights[i])
+                # print "contour ", i, " probability: ", contour_weights[
+                #     i], " chosen: ", chosen_random[
+                #         i], " distance: ", avg_depth[i]
+
+            #
+            # chosen_random = np.random.choice(
+            #     range(0, len(contours)),
+            #     size=len(contours),
+            #     replace=False,
+            #     p=avg_depth_probabilities)
+
+            chosen = np.bitwise_or(chosen_random, chosen_area)
+
+            print "selected ", np.count_nonzero(
+                chosen_random), " contours at random."
+            print "selected ", np.count_nonzero(
+                chosen_area), " contours because of small area."
+
+            print "selected ", np.count_nonzero(chosen), " in total."
 
             mask = np.zeros(depth_mask.shape, np.uint8)
-            cv2.drawContours(mask, [c for i, c in enumerate(contours) if chosen[i]], -1, 255, -1)
+            cv2.drawContours(mask,
+                             [c for i, c in enumerate(contours) if chosen[i]],
+                             -1, 255, -1)
 
             return np.bitwise_and(depth_mask, mask != 0)
 
@@ -224,8 +357,8 @@ class Contours(DenseToSparse):
 class Superpixels(DenseToSparse):
     name = "superpixels"
 
-    def __init__(self, num_samples):
-        DenseToSparse.__init__(self)
+    def __init__(self, num_samples, apply_kinect_noise=True):
+        DenseToSparse.__init__(self, apply_kinect_noise)
         self.num_samples = num_samples
 
     def __repr__(self):
@@ -233,9 +366,13 @@ class Superpixels(DenseToSparse):
                (self.name, self.num_samples)
 
     def dense_to_sparse(self, rgb, depth):
+        if self.apply_kinect_noise:
+            depth = DenseToSparse.apply_kinect_noise(self, depth)
+
         depth_mask = depth != 0.0
 
-        seeds = cv2.ximgproc.createSuperpixelSEEDS(np.size(rgb, 1), np.size(rgb, 0), 3, self.num_samples, 8)
+        seeds = cv2.ximgproc.createSuperpixelSEEDS(
+            np.size(rgb, 1), np.size(rgb, 0), 3, self.num_samples, 8)
 
         seeds.iterate(rgb, 8)
 
@@ -245,13 +382,15 @@ class Superpixels(DenseToSparse):
 
         randomness = "uar"
         if randomness == "uar":
-            choices = np.random.choice([0, 1], size=self.num_samples, replace=True, p=[0.1, 0.9])
+            choices = np.random.choice(
+                [0, 1], size=self.num_samples, replace=True, p=[0.1, 0.9])
             chosen = [i for i, choice in enumerate(choices) if choice == 1]
         else:
             # Thinking in process here...
             probs = np.ones(np.shape(depth_mask), np.uint32)
             chosen = []
-            choice = np.random.choice(num_labels, size=1, replace=False, p=probs)
+            choice = np.random.choice(
+                num_labels, size=1, replace=False, p=probs)
 
             chosen_pixels = labels_out == choice
 
@@ -264,4 +403,3 @@ class Superpixels(DenseToSparse):
             mask[labels_out == i] = 1
 
         return np.bitwise_and(depth_mask, mask)
-
