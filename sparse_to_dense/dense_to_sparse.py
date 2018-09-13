40441939c79c9360cacb2b438ee2cd87ce25db05
Tim Taubner <taubnert@ethz.ch>
2018-09-12 18:11:03 -0700

    Small fix to Dilatation

--- sparse_to_dense/dense_to_sparse.py
+++ sparse_to_dense/dense_to_sparse.py
@@ -115,57 +115,29 @@ class SimulatedStereo(DenseToSparse):
     # Threshold the edge gradient
     # Dilatate
     def depth_mask(self, rgb, depth):
-        gray = rgb2grayscale(rgb)
-
         depth_mask = np.bitwise_and(depth != 0.0, depth <= self.max_depth)
 
-        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-        gx = cv2.Sobel(blurred, cv2.CV_64F, 1, 0, ksize=5)
-        gy = cv2.Sobel(blurred, cv2.CV_64F, 0, 1, ksize=5)
-
-        mag = cv2.magnitude(gx, gy)
         non_zeros = np.count_nonzero(depth_mask)
 
         if non_zeros > 0:
+            gray = rgb2grayscale(rgb)
+            blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+            gx = cv2.Sobel(blurred, cv2.CV_64F, 1, 0, ksize=5)
+            gy = cv2.Sobel(blurred, cv2.CV_64F, 0, 1, ksize=5)
+
+            mag = cv2.magnitude(gx, gy)
             target_samples = 1.0 * self.num_samples / np.size(depth) * non_zeros
             edge_percentage = float(100 * target_samples) / np.size(depth)
-            min_mag_lower = np.percentile(
-                mag[depth_mask], 100 - edge_percentage, interpolation='lower')
             min_mag_upper = np.percentile(
                 mag[depth_mask], 100 - edge_percentage, interpolation='higher')
-            # print("Upper threshold %f with %d pixels above it" %
-            #      (min_mag_upper, np.count_nonzero(np.bitwise_and(depth_mask, mag >= min_mag_upper))))
-            # print("Lower threshold %f with %d pixels above it" %
-            #      (min_mag_lower, np.count_nonzero(np.bitwise_and(depth_mask, mag >= min_mag_lower))))
             mag_mask = np.bitwise_and(mag >= min_mag_upper, depth_mask)
 
-            missing_samples = target_samples - np.count_nonzero(mag_mask)
-            # assert missing_samples >= 0
-            #if missing_samples > 0:
-            # print("Missing %d samples" % missing_samples)
-            # If we take all of mag_fill_mask we have more than desired number of samples
-            #    mag_fill_mask = np.bitwise_and(depth_mask, mag < min_mag_upper, mag >= min_mag_lower)
-            #    idx_0, idx_1 = np.nonzero(mag_fill_mask)
-            #    potential_samples = len(idx_0)
-            # print("Got %d potential samples" % potential_samples)
-            #    if potential_samples > missing_samples:
-            # We choose some random samples to fill in
-            #        index_of_indices = np.random.choice(len(idx_0), missing_samples, replace=False)
-            # Set these randomly chosen potentials to 1
-            #        mag_mask[idx_0[index_of_indices], idx_1[index_of_indices]] = 1
-            # The threshold is still the lowest possible
-            #    else:
-            #        mag_mask[idx_0, idx_1] = 1
-            # print("filling up only partly possible, as all pixels with depth <= max_depth are already taken")
-            #        assert np.all(mag_mask[depth_mask] == 1)
-
             if self.dilate_iterations >= 0:
-                kernel = np.ones(
-                    (self.dilate_kernel, self.dilate_kernel), dtype=np.uint8)
-                cv2.dilate(
+                kernel = np.ones((self.dilate_kernel, self.dilate_kernel), dtype=np.uint8)
+                return cv2.dilate(
                     mag_mask.astype(np.uint8),
                     kernel,
-                    iterations=self.dilate_iterations)
+                    iterations=self.dilate_iterations) > 0
 
             return mag_mask
         else:
